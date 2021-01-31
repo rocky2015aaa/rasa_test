5d8cc52532e5569454a680be0ba56f81612d3b94
Donggeon Lee <rocky2010aaa@gmail.com>
2021-01-31 10:12:18 +0900

    working on livechat

--- actions/actions.py
+++ actions/actions.py
@@ -6,22 +6,114 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Text, Dict, Any, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import (
+    SlotSet,
+    AllSlotsReset,
+    ConversationPaused,
+    ConversationResumed,
+    FollowupAction,
+    UserUtteranceReverted,
+)
+import requests
+import json
+from random import randint
+import datetime
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+"""
+class ActionTalkToHuman(Action):
+    def name(self) -> Text:
+        return "action_talk_to_human"
+
+    def run(self, dispatcher, tracker: Tracker, domain):
+        print("***", tracker.sender_id)
+        response = "Reaching out to a human agent [{}]...".format(tracker.sender_id)
+        dispatcher.utter_message(response)
+        ConversationPaused()
+        message = ""
+        while message != "/unpause":
+            url = "http://127.0.0.1:5000/handoff/{}".format(tracker.sender_id)
+            req = requests.get(url)
+            resp = json.loads(req.text)
+            if "error" in resp:
+                raise Exception("Error fetching message: " + repr(resp["error"]))
+            message = resp["message"]
+            print(message)
+            if message != "/unpause":
+                dispatcher.utter_message("Human agent: {}".format(message))
+
+        return [ConversationResumed()]
+class ActionTalkToHuman(Action):
+    def name(self) -> Text:
+        return "action_talk_to_human"
+
+    def run(self, dispatcher, tracker: Tracker, domain):
+        response = "Reaching out to a human agent [{}]...".format(tracker.sender_id)
+        dispatcher.utter_message(response)
+        return [ConversationPaused()]
+class ActionTalkToHuman(Action):
+    def name(self) -> Text:
+        return "action_talk_to_human"
+
+    def run(self, dispatcher, tracker: Tracker, domain):
+        url = "http://127.0.0.1:5000/handoff/123"
+        req = requests.get(url)
+        resp = json.loads(req.text)
+        if "error" in resp:
+            raise Exception("Error fetching message: " + repr(resp["error"]))
+        message = resp["message"]
+        dispatcher.utter_message("Human agent: {}".format(message))
+        return []
+
+class ActionTalkingToHuman(Action):
+    def name(self) -> Text:
+        return "action_talking_to_human"
+
+    def run(self, dispatcher, tracker: Tracker, domain):
+        sender_id = tracker.sender_id
+        dispatcher.utter_message(
+            f"Resumed this conversation, with ID: " f"{sender_id}."
+        )
+        return [ConversationResumed()]
+"""
+class ActionTalkToUser(Action):
+    def name(self) -> Text:
+        return "action_talk_to_user"
+
+    def run(self, dispatcher, tracker: Tracker, domain):
+        message = ""
+        while message == "":
+            url = "http://127.0.0.1:5000/handoff/123"
+            req = requests.get(url)
+            resp = json.loads(req.text)
+            if "error" in resp:
+                raise Exception("Error fetching message: " + repr(resp["error"]))
+            message = resp["message"]
+        dispatcher.utter_message("Human agent: {}".format(message))
+        return []
+
+class ActionToHumanAgent(Action):
+    def name(self) -> Text:
+        return "action_talk_to_human_agent"
+
+    def run(self, dispatcher, tracker: Tracker, domain):
+        message = ""
+        while message == "":
+            message = tracker.latest_message['text']
+            url = "http://127.0.0.1:5000/handoff/123"
+            data = {'message': message}
+            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+            req = requests.post(url, data=json.dumps(data), headers=headers)
+            resp = json.loads(req.text)
+            if "error" in resp:
+                raise Exception("Error fetching message: " + repr(resp["error"]))
+        return []
---
+++ humanchat/humanchat.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+import time
+
+app = Flask(__name__)
+
+sender_store = {"123": {"paused": True, "replies": []}}
+
+@app.route('/handoff/<sender_id>', methods=["GET","POST"])
+def handoff(sender_id):
+    if request.method == "POST":
+        print(request.get_json()["message"])
+        if sender_id in sender_store:
+            sender_store[sender_id]["replies"].append(request.get_json()["message"])
+            return jsonify(sender_store[sender_id])
+        else:
+            return jsonify({"error": "sender_id not found"})
+    else:
+        if sender_id not in sender_store:
+            sender_store[sender_id] = {"paused": True, "replies": []}
+        
+        while len(sender_store[sender_id]["replies"]) == 0:
+            time.sleep(1)
+
+        message = sender_store[sender_id]["replies"].pop(0)
+        return jsonify({"message": message})
+
+
+if __name__ == '__main__':
+    app.run()
